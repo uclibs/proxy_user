7d87dc08aab0808239b8fc12ac5ca6921328438d
Thomas Scherz <scherztc@ucmail.uc.edu>
2020-02-11 15:47:23 -0500

    Adds URL redirect.

---
+++ redirect.php
@@ -0,0 +1,4 @@
+<?php
+  $username = $_GET["username"];
+  header("Location: https://login.proxy.libraries.uc.edu/audit?date=365&anyall=all&field1=event&cond1=c&value1=&field2=username&cond2=c&value2=$username&field3=ip&cond3=c&value3=&field4=location&cond4=c&value4=&field5=event&cond5=c&value5=&field6=event&cond6=c&value6=&field7=event&cond7=c&value7=&field8=event&cond8=c&value8=&field9=event&cond9=c&value9=&field10=event&cond10=c&value10=&field11=event&cond11=c&value11=&field12=event&cond12=c&value12=&field13=event&cond13=c&value13=&field14=event&cond14=c&value14=&field15=event&cond15=c&value15=&field16=event&cond16=c&value16=&field17=event&cond17=c&value17=&field18=event&cond18=c&value18=&field19=event&cond19=c&value19=&field20=event&cond20=c&value20=&search=Search");
+?>
